09a623f0702b7e778d5070d7c0a353ca545faceb
rizkiram123 <rizkiramadhan200908@gmail.com>
2025-07-28 14:59:21 +0700

    Latihan algoritma

---
+++ R.js
@@ -0,0 +1,79 @@
+// function sapa () {
+//     return {
+//         name: 'sapa',
+//         version: '1.0.0',
+//         description: 'A simple JavaScript library for building user interfaces.',
+//     }
+// }
+
+// sapa()
+
+// function tambah (a, b) {
+//     return a + b;
+// }
+
+// let hasil = tambah(5, 3);
+// console.log("hasil:" + hasil);
+
+// let car = {
+//     nama: "Toyota",
+//     model: "Corolla",
+//     weight: 1500,
+//     color: "Merah",
+//     year: 2020,
+
+//     start: function() {
+//         console.log("Mobil dimulai");
+//     },
+//     drive: function() {
+//         console.log("Mobil berjalan");
+//     },
+//     brake: function() {
+//         console.log("Mobil mengerem");
+//     },
+//     stop: function() {
+//         console.log("Mobil dimatikan");
+//     },
+
+// };
+//     console.log (car.nama);
+//     console.log (car.model);
+//     console.log (car.weight);
+//     console.log (car.color);
+//     console.log (car.year);
+
+//     car.start();
+//     car.drive();
+//     car.brake();
+//     car.stop();
+
+
+    let byc = {
+    nama: "Polygon",
+    jumgi: 21,
+    warna: "Merah",
+    
+    start: function() {
+        console.log("Sepeda di gayuh");
+    },
+    drive: function() {
+        console.log("sepeda berjalan");
+    },
+    brake: function() {
+        console.log("sepeda mengerem");
+    },
+    stop: function() {
+        console.log("sepeda berenti");
+    },
+
+};
+    console.log (byc.nama);
+    console.log (byc.jumgi);
+    console.log (byc.warna);
+   
+
+    byc.start();
+    byc.drive();
+    byc.brake();
+    byc.stop();
+
